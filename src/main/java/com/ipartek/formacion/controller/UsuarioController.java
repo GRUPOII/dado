a0682a7642e955c58d8e8f442519a5bd62f210c1
Itsaso Aguirre <taz_788@hotmail.com>
2017-03-06 11:41:10 +0100

    Creacion de usuarios

--- src/main/java/com/ipartek/formacion/controller/UsuarioController.java
+++ src/main/java/com/ipartek/formacion/controller/UsuarioController.java
@@ -5,9 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ipartek.formacion.domain.Usuario;
 import com.ipartek.formacion.service.ServiceUsuario;
 
 /**
@@ -38,6 +41,44 @@ public class UsuarioController {
 		model.addAttribute("usuarios", this.serviceUsuario.listar());
 
 		return "usuario/index";
+	}
+
+	/**
+	 * 
+	 * @param usuario
+	 * @param bindingResult
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/usuario/crear", method = RequestMethod.POST)
+	public String crear(@Validated Usuario usuario, BindingResult bindingResult, Model model) {
+
+		LOG.info("recibimos datos del formulario " + usuario);
+		String msg = null;
+
+		// validar datos del formulario
+		if (!bindingResult.hasErrors()) {
+
+			if (usuario.getId() == -1) {
+
+				serviceUsuario.crear(usuario);
+				msg = "Creado nuevo Usuario";
+
+			} else {
+
+				serviceUsuario.modificar(usuario);
+				msg = "Modificado Usuario";
+
+			}
+
+		} else {
+
+			LOG.info("formulario con datos no validos");
+
+		}
+
+		model.addAttribute("msg", msg);
+		return "usuario/form";
 	}
 
 }
--- src/main/java/com/ipartek/formacion/repository/DAOUsuarioImpl.java
+++ src/main/java/com/ipartek/formacion/repository/DAOUsuarioImpl.java
@@ -1,5 +1,9 @@
 package com.ipartek.formacion.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.ipartek.formacion.domain.Usuario;
@@ -25,7 +32,7 @@ import com.ipartek.formacion.repository.mapper.UsuarioMapper;
 @Repository(value = "daoUsuario")
 public class DAOUsuarioImpl implements DAOUsuario {
 
-	private final Log logger = LogFactory.getLog(getClass());
+	private final Log LOG = LogFactory.getLog(getClass());
 
 	@Autowired()
 	private DataSource dataSource;
@@ -42,6 +49,7 @@ public class DAOUsuarioImpl implements DAOUsuario {
 
 	private static final String SQL_GET_ALL = "SELECT `id`, `nombre`, `fecha_alta`, `fecha_modificacion`,`fecha_baja` FROM `usuario` ORDER BY `id` ASC LIMIT 500;";
 	private static final String SQL_GET_BY_ID = "SELECT `id`, `nombre`, `fecha_alta`, `fecha_modificacion`,`fecha_baja` FROM `usuario` WHERE `id` = ?";
+	private static final String SQL_INSERT = "INSERT INTO `dado`.`usuario` (`nombre`) VALUES (?);";
 
 	@Override()
 	public List<Usuario> getAll() {
@@ -53,11 +61,11 @@ public class DAOUsuarioImpl implements DAOUsuario {
 
 		} catch (EmptyResultDataAccessException e) {
 
-			this.logger.warn("No existen usuarios todavia");
+			this.LOG.warn("No existen usuarios todavia");
 
 		} catch (Exception e) {
 
-			this.logger.error(e.getMessage());
+			this.LOG.error(e.getMessage());
 
 		}
 		return lista;
@@ -73,11 +81,11 @@ public class DAOUsuarioImpl implements DAOUsuario {
 
 		} catch (EmptyResultDataAccessException e) {
 
-			this.logger.warn("No existen usuarios todavia");
+			this.LOG.warn("No existen usuarios todavia");
 
 		} catch (Exception e) {
 
-			this.logger.error(e.getMessage());
+			this.LOG.error(e.getMessage());
 
 		}
 
@@ -85,9 +93,37 @@ public class DAOUsuarioImpl implements DAOUsuario {
 	}
 
 	@Override()
-	public boolean insert(Usuario u) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean insert(final Usuario u) {
+
+		this.LOG.trace("insert " + u);
+		boolean resul = false;
+
+		try {
+			int affectedeRows = -1;
+			KeyHolder keyHolder = new GeneratedKeyHolder();
+
+			affectedeRows = this.jdbcTemplate.update(new PreparedStatementCreator() {
+
+				@Override()
+				public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+					PreparedStatement ps = conn.prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
+					ps.setString(1, u.getNombre());
+
+					return ps;
+				}
+			}, keyHolder);
+
+			if (affectedeRows == 1) {
+				u.setId((int) keyHolder.getKey().longValue());
+				resul = true;
+			}
+		} catch (Exception e) {
+
+			this.LOG.error(e.getMessage());
+
+		}
+
+		return resul;
 	}
 
 	@Override()
