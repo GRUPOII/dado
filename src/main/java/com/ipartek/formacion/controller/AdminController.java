fbedfa56f4ac20442c2dfb3f0a09aa221229a302
Iván Delgado <delgadogarciaivan@gmail.com>
2017-03-07 10:05:53 +0100

    Modificada página de administracion

--- src/main/java/com/ipartek/formacion/controller/AdminController.java
+++ src/main/java/com/ipartek/formacion/controller/AdminController.java
@@ -2,10 +2,18 @@ package com.ipartek.formacion.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ipartek.formacion.domain.Usuario;
+import com.ipartek.formacion.service.ServiceUsuario;
 
 /**
  * Contolador para el admin
@@ -18,19 +26,127 @@ public class AdminController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(AdminController.class);
 
+	@Autowired()
+	private ServiceUsuario serviceUsuario;
+
 	/**
 	 * Listar usuarios
 	 * 
 	 * @param model
-	 *            modelo
-	 * @return lista de usuarios
+	 *            atributos
+	 * @return listado de usuarios
 	 */
 	@RequestMapping(value = "/admin", method = RequestMethod.GET)
 	public String listar(Model model) {
-		LOG.info("");
-		// TODO recuperar usuarios desde servicio, pasar como atributo en el
-		// modelo
-		return "admin/index";
+		LOG.info("listar");
+
+		model.addAttribute("usuarios", this.serviceUsuario.listar());
+
+		return "usuario/index";
 	}
 
+	/**
+	 * Eliminar usuarios
+	 * 
+	 * @param id
+	 *            selecciona el id del usuario a eliminar
+	 * @param model
+	 *            atributos
+	 * @return a la vista de usuarios con el usuario eliminado
+	 */
+	@RequestMapping(value = "/admin/usuario/delete/{id}", method = RequestMethod.GET)
+	public String eliminar(@PathVariable() int id, Model model) {
+
+		LOG.info("eliminar usuario " + id);
+		String msg = "Usuario no eliminado";
+
+		try {
+			if (this.serviceUsuario.eliminar(id)) {
+				msg = "Usuario Eliminada con exito";
+			}
+		} catch (DataIntegrityViolationException e) {
+			msg = e.getMessage();
+		}
+		model.addAttribute("msg", msg);
+		model.addAttribute("usuarios", this.serviceUsuario.listar());
+
+		return "usuario/index";
+	}
+
+	/**
+	 * Editamos un usuario, ya sea mofiicar o crear
+	 * 
+	 * @param model
+	 *            atributos que tenemos en la JSP
+	 * @return vista del formulario de usuarios
+	 */
+
+	@RequestMapping(value = "/admin/usuario/edit", method = RequestMethod.GET)
+	public String irFormularioNuevo(Model model) {
+
+		model.addAttribute("usuario", new Usuario());
+		return "admin/form";
+	}
+
+	/**
+	 * Detalle de un usuario concreto
+	 * 
+	 * @param id
+	 *            del usuario
+	 * @param model
+	 *            atributos de la JSP
+	 * @return vista del detalle de ese usuario
+	 */
+	@RequestMapping(value = "/admin/usuario/edit/{id}", method = RequestMethod.GET)
+	public String irFormularioEditar(@PathVariable() int id, Model model) {
+
+		Usuario usuario = this.serviceUsuario.buscarPorId(id);
+
+		model.addAttribute("usuario", usuario);
+		return "admin/form";
+	}
+
+	/**
+	 * 
+	 * Crear usuario
+	 * 
+	 * @param usuario
+	 *            parametro para nuevo usuario
+	 * @param bindingResult
+	 *            conexion
+	 * @param model
+	 *            modelo
+	 * @return devuelve el listado con un usuario nuevo
+	 */
+	@RequestMapping(value = "/admin/usuario/crear", method = RequestMethod.POST)
+	public String crear(@Validated() Usuario usuario, BindingResult bindingResult, Model model) {
+
+		LOG.info("recibimos datos del formulario " + usuario);
+		String msg = null;
+
+		// validar datos del formulario
+		if (!bindingResult.hasErrors()) {
+
+			if (usuario.getId() == -1) {
+
+				this.serviceUsuario.crear(usuario);
+				msg = "Creado nuevo Usuario";
+
+			} else {
+
+				this.serviceUsuario.modificar(usuario);
+				msg = "Modificado Usuario";
+
+			}
+
+		} else {
+
+			LOG.info("formulario con datos no validos");
+
+		}
+
+		model.addAttribute("msg", msg);
+		return "admin/form";
+
+	}
 }
--- src/main/webapp/WEB-INF/views/includes/header.jsp
+++ src/main/webapp/WEB-INF/views/includes/header.jsp
@@ -5,68 +5,58 @@
 <%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 
-<%@ page session="false" %>
+<%@ page session="false"%>
 
 <html>
 <head>
-	<title>Dado-eq2</title>
-	<base href="/dado-eq2/" /> 
-	
-	
-	<link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
-	<link href="https://cdn.datatables.net/1.10.13/css/dataTables.bootstrap.min.css" rel="stylesheet">
-	
-	<!-- Custom -->
-	<link href="resources/css/custom.css" rel="stylesheet">
-	
-	
+<title>Dado-eq2</title>
+<base href="/dado-eq2/" />
+
+
+<link href="http://getbootstrap.com/dist/css/bootstrap.min.css"
+	rel="stylesheet">
+<link
+	href="https://cdn.datatables.net/1.10.13/css/dataTables.bootstrap.min.css"
+	rel="stylesheet">
+
+<!-- Custom -->
+<link href="resources/css/custom.css" rel="stylesheet">
+
+
 </head>
 <body>
 
-	
-	 <div class="container">
 
-      <!-- Static navbar -->
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Web Dado-eq2</a>
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-              <li><a href="estadisticas">Estadísticas</a></li>   
-            </ul>
-            <ul class="nav navbar-nav">
-              <li><a href="usuario">Usuarios</a></li>   
-            </ul>
-            
-            <!-- Menu de la derecha -->
-            <ul class="nav navbar-nav navbar-right">
-             <li><a href="admin">Login</a></li>
-            </ul> 
-                    
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
-      
+	<div class="container">
 
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        
-      
+		<!-- Static navbar -->
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed"
+						data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+						aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span> <span
+							class="icon-bar"></span> <span class="icon-bar"></span> <span
+							class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Web Dado-eq2</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="estadisticas">Estadísticas</a></li>
+					</ul>
+					<!-- Menu de la derecha -->
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="admin">Administracion</a></li>
+					</ul>
 
-   
-	
-	
-	
-	
-	
-	
-	
-	
+				</div>
+				<!--/.nav-collapse -->
+			</div>
+			<!--/.container-fluid -->
+		</nav>
+
+
+		<!-- Main component for a primary marketing message or call to action -->
+		<div class="jumbotron">
